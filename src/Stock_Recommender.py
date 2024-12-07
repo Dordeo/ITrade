9769cca544e2aaa05883417d9dc6076070d11d63
Dordeo <dordeomeyrin@gmail.com>
2024-12-07 22:30:39 +0100

    Made a unique function that selects a specific number of unique elements in the dataframe and returns a sorted dataframe.

--- src/Stock_Recommender.py
+++ src/Stock_Recommender.py
@@ -32,6 +32,60 @@ class stock_recommender:
         df = df.drop(columns=["CompoundScore"]);
 
         return df;
+
+    def get_unique(self, df, n) -> dataframe:
+
+        if n > len(df):
+            return df;
+
+        unique_industries = df["Industry"].unique();
+        # unique_ industries is a numpy_array that contains all unique industries
+
+        # this is going to be a list of dataframes containing solely a specific industry
+        partition: list[dataframe] = []
+
+        # this list is of integers that track the length of each df in partition
+        lengths = [];
+        #this list will track how much elements we take from each df in partition
+        takes = [];
+        #i is just an index
+        i = 0;
+
+        for element in unique_industries:
+            partition.append(
+                df[df["Industry"].str.contains(element, case=False)]
+            )
+
+        for dfa in partition:
+            lengths.append(len(dfa));
+            takes.append(0);
+
+        #while i is less than n
+        while i < n:
+
+            # I go through each element in length
+            # and if it is not 0 i transfer 1 from lengths to takes
+            for j in range(len(lengths)):
+                if (i < n):
+
+                    if (lengths[j] != 0):
+                        lengths[j] -= 1;
+                        takes[j] += 1;
+                        i += 1;
+
+
+        i = 0;
+        res = pd.DataFrame();
+
+        # now I go thorugh each df in partition and add respective amount of elements determined in takes[i] to
+        # res dataframe
+
+        for dfa in partition:
+            res = pd.concat([res, dfa.head(takes[i])], ignore_index=True);
+            i += 1;
+
+        return res
+
     def get_stocks(self, user: user_profile, n: int) -> dataframe:
 
         # according to my algorythm we are first going to exclude some stocks based on their establishment year
@@ -71,63 +125,6 @@ class stock_recommender:
         # now however I want to include all unique industries at least once as long as it is in confines
         # of number of stocks the user entered
 
-        unique_industries = self.tdf["Industry"].unique();
-        # unique_ industries is a numpy_array that contains all unique industries
-
-        #this is going to be a list of dataframes containing solely a specific industry
-        partition: list[dataframe] = []
-        #this list is of integers that track the length of each df in partition
-        lengths = [];
-        takes = [];
-        i = 0;
-
-
-        for element in unique_industries:
-            partition.append(
-                self.tdf[self.tdf["Industry"].str.contains(element, case = False)]
-            )
-
-        for df in partition:
-            lengths.append(len(df));
-            takes.append(0);
-
-        i = 0;
-        # now we have sorted all those unique industries stocks so that the top contendors are on the top
-        # however now we run into another problem, it is not guaranteed that all df in partition are of equal size
-        # and we need to include all industries once unless it is out of bound of n.
-        # so now I am going to do this. Create variable cycle_tracker -> tracks the main loop cycle
-        # I am also going to have a sub loop which is going to parse through partition and select
-        # each df in partition once per main loop cycle. in the sub loop, I am going to access an element in each df
-        # under index of cycle_tracker. if it is accesible I am going to add that row into ...
-
-        # Actually no. this will lead to multiple redundacies I would rather do formulas
-        # Scrap that instead I am going to simplify it into a simple integer problem
-
-        if n > len(self.tdf):
-            print("There were not enough stocks, Please lower your requirments of number of stocks")
-            return self.tdf;
-
-        # here the problem begins lengths track the number of elements in each df.
-        # takes indicate how much elements I take from each df
-
-        # here it goes
-
-        while i < n:
-
-            for j in range(len(lengths)):
-                if(i < n):
-
-                    if (lengths[j] != 0):
-                        lengths[j] -= 1;
-                        takes[j] += 1;
-                        i+=1;
-
-
-        i = 0;
-        res = pd.DataFrame();
-        for df in partition:
-            res = pd.concat([res, df.head(takes[i])], ignore_index=True);
-            i+=1;
 
         res = res.sort_values(by = "Performance", ascending=False);
 
